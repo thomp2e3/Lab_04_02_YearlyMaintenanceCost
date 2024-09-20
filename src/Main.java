2c68c1943fb8bdb0a2c87acaa1004139a39aaec9
thomp2e3 <thomp2e3@mail.uc.edu>
2024-09-19 22:29:21 -0400

    completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double winterCost = 50;
+        double springCost = 300;
+        double summerCost = 150;
+        double fallCost = 45;
+        double yearlyCost = 0;
+
+
+
+        yearlyCost = winterCost + springCost + summerCost + fallCost;
+        System.out.println("Your yearly home maintenance cost is the winter cost " + winterCost + " + the spring cost " + springCost + " + the summer cost " + summerCost + " + and the fall cost " + fallCost + " equals " + yearlyCost);
+
     }
 }
